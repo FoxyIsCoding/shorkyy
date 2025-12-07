73e431119dc6fe83728e345e62a6145a8b883b8c
FoxyIsCoding <77046186+FoxyIsCoding@users.noreply.github.com>
2025-12-07 16:52:51 +0100

    preparation for shop mechanic

--- scripts/shop/main_controller.gd
+++ scripts/shop/main_controller.gd
@@ -3,11 +3,11 @@ extends Control
 var clicks: int
 const SAVE_PATH = "user://click_data.save"
 
-
 func _ready():
 	load_stats()
 	$HBoxContainer/cookies.text = clicks
 	
+
 func _on_texture_button_pressed():
 	get_tree().change_scene_to_file("res://scenes/map.tscn")
 	
@@ -29,23 +29,19 @@ func load_stats():
 			clicks = save_data.get("clicks", 0)
 			print("Loaded clicks: ", clicks)
 
-# hover effects 
 
-func _on_pointer_mouse_entered():
-	var tween = create_tween()
-	tween.set_parallel(true)
-	
-	# Shrink the object to 90% of its size
-	tween.tween_property(self, "scale", scale * 0.9, 0.2)
-	
-	# Rotate to the right (15 degrees)
-	tween.tween_property(self, "rotation_degrees", rotation_degrees + 15, 0.2)
-	
-	# Chain the return animations
-	tween.set_parallel(false)
-	
-	# Return to original scale
-	tween.tween_property(self, "scale", scale / 0.9, 0.2)
-	
-	# Return to original rotation
-	tween.tween_property(self, "rotation_degrees", rotation_degrees - 15, 0.2)
+# ------------------------------------------------------------------------------------------------------
+# Shop vars + exports
+# ------------------------------------------------------------------------------------------------------
+@export_category("Shop")
+@export_subgroup("Pointer")
+@export var pointer_price: int
+@export_subgroup("test")
+@export var femboys_price: int
+@export_subgroup("test")
+@export var golden_cookie_price: int
+@export_subgroup("test")
+@export var blahaj_price: int
+@export_subgroup("test")
+@export var useless_coin_price: int
+@export_subgroup("test")
