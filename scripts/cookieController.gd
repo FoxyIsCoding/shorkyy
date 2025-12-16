bc2dc841663518f11868801ecd2bd41ff5ab034c
FoxyIsCoding <77046186+FoxyIsCoding@users.noreply.github.com>
2025-12-16 17:02:01 +0100

    theming that thing or whatever i did

--- scripts/cookieController.gd
+++ scripts/cookieController.gd
@@ -26,7 +26,6 @@ func _ready():
 	$"../cps".text = str(cps) + "CPS"
 	$"../cps".text = str(controller.cps) + "CPS"
 	$"../clicks".text = str(controller.clicks)
-	femboy_repeater(controller.femboys_owned)
 	
 
 
@@ -65,65 +64,3 @@ func _on_button_up():
 
 func _on_tween_done():
 	pass
-
-func femboy_repeater(count: int):
-	var container = $"../misc/repeater"
-	
-	if container == null:
-		print("Error: misc/repeater not found!")
-		return
-	
-	if container is HBoxContainer:
-		container.alignment = BoxContainer.ALIGNMENT_BEGIN
-	
-	for child in container.get_children():
-		if child.name.begins_with("clone_"):
-			child.queue_free()
-	
-
-	var placeholders = []
-	if container.has_node("placeholder"):
-		placeholders.append(container.get_node("placeholder"))
-	for i in range(2, 9):
-		var node_name = "placeholder" + str(i)
-		if container.has_node(node_name):
-			placeholders.append(container.get_node(node_name))
-	
-	if placeholders.size() == 0:
-		print("Error: No placeholder nodes found!")
-		return
-	
-
-	for placeholder in placeholders:
-		placeholder.visible = false
-	
-	var max_width = get_viewport_rect().size.x - 40
-	var separation = 1
-	container.add_theme_constant_override("separation", separation)
-	
-	var total_separation = separation * (count - 1)
-	var available_width = max_width - total_separation
-	
-	var original = placeholders[0]
-	var original_width = original.size.x if original.size.x > 0 else 100
-	var original_height = original.size.y if original.size.y > 0 else 100
-	var aspect_ratio = original_height / original_width
-
-	var new_width = min(available_width / count, original_width)
-	var new_height = new_width * aspect_ratio
-	
-	for i in range(count):
-		var placeholder_index = i % placeholders.size()
-		var clone = placeholders[placeholder_index].duplicate()
-		clone.name = "clone_" + str(i + 1)
-		clone.visible = true
-		clone.custom_minimum_size = Vector2(new_width, new_height)
-		clone.size = Vector2(new_width, new_height)
-		clone.size_flags_horizontal = Control.SIZE_SHRINK_BEGIN
-		clone.size_flags_vertical = Control.SIZE_SHRINK_END
-		
-		if clone is TextureRect:
-			clone.expand_mode = TextureRect.EXPAND_IGNORE_SIZE
-			clone.stretch_mode = TextureRect.STRETCH_KEEP_ASPECT_CENTERED
-		
-		container.add_child(clone)
