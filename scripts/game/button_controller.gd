21eb54e5a61db06834238d3e8e4c3d3d2afdce78
FoxyIsCoding <77046186+FoxyIsCoding@users.noreply.github.com>
2025-12-17 16:37:26 +0100

    peak shi sidebar

--- scripts/game/button_controller.gd
+++ scripts/game/button_controller.gd
@@ -1,4 +1,4 @@
-extends Node2D
+extends Control
 var controller = load("res://scripts/shop/main_controller.gd")
 func _ready():
 	$cps.text = str(controller.pointer_owned) + "CPS"
@@ -13,3 +13,13 @@ func _on_menu_btn_pressed():
 	$audio/button.play()
 	get_tree().change_scene_to_file("res://scenes/menu.tscn")
 		   
+
+
+func _on_content_mouse_entered():
+	if $AnimationPlayer.current_animation == "sidebar_slide_out" or "RESET":
+		$AnimationPlayer.play("sidebar_slide_in")
+
+
+func _on_content_mouse_exited():
+	if $AnimationPlayer.current_animation == "sidebar_slide_in":
+		$AnimationPlayer.play("sidebar_slide_out")	
